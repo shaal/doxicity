718c7df7581b5d64f1dd91dd3faf46dfd55aaf8d
Cory LaViska <cory@abeautifulsite.net>
2022-04-17 12:31:00 -0400

    update comment

--- src/plugins/beautify/beautify.ts
+++ src/plugins/beautify/beautify.ts
@@ -2,7 +2,10 @@ import beautify from 'js-beautify';
 import type { HTMLBeautifyOptions } from 'js-beautify';
 import type { DoxicityPlugin } from 'src/utilities/types';
 
-/** The Doxicity beautify plugin. */
+/**
+ * Beautifies markup by re-indenting and lightly reformatting the HTML. This plugin should run after all other
+ * transformations to ensure it runs on the final version of the HTML.
+ */
 export default function (options: Partial<HTMLBeautifyOptions>): DoxicityPlugin {
   return {
     afterTransform: (html: string) => {
