a3245ac54bbe3445c1dcb3a6bbaf56f4e89986f2
Cory LaViska <cory@abeautifulsite.net>
2022-04-23 11:25:00 -0400

    add dashes and ellipses

--- src/plugins/smart-quotes/smart-quotes.ts
+++ src/plugins/smart-quotes/smart-quotes.ts
@@ -1,13 +1,19 @@
 /* @ts-expect-error - no types */
-import smartQuotes from 'smartquotes';
+import smartquotes from 'smartquotes';
 import type { DoxicityPlugin } from 'src/utilities/types';
+
+/* eslint-disable */
+smartquotes.replacements.push([/\-\-\-/g, '\u2014']); // em dash
+smartquotes.replacements.push([/\-\-/g, '\u2013']); // en dash
+smartquotes.replacements.push([/\.\.\./g, '\u2026']); // ellipses
+/* eslint-enable */
 
 interface SmartQuotesOptions {
   /** A selector that matches one or more elements to apply smart quotes to. Defaults to "main". */
   selector: string;
 }
 
-/** Adds smart quotes to selected content. */
+/** Adds smart quotes and some typography enhancements to selected content. */
 export default function (options: Partial<SmartQuotesOptions>): DoxicityPlugin {
   options = {
     selector: 'main',
@@ -18,7 +24,7 @@ export default function (options: Partial<SmartQuotesOptions>): DoxicityPlugin {
     transform: doc => {
       const elements = [...doc.querySelectorAll(options.selector!)];
       // eslint-disable-next-line
-      elements.forEach(el => smartQuotes.element(el));
+      elements.forEach(el => smartquotes.element(el));
       return doc;
     }
   };
