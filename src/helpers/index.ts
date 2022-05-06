a6c0940f995813a37cf83c1348ac0eb75b48d1f5
Cory LaViska <cory@abeautifulsite.net>
2022-05-06 09:43:02 -0400

    export plugins and helpers from index

---
+++ src/helpers/index.ts
@@ -0,0 +1,3 @@
+export { default as formatDateHelper } from './format-date/format-date.js';
+export { default as formatNumberHelper } from './format-number/format-number.js';
+export { default as formatUrlHelper } from './format-url/format-url.js';
---
+++ src/plugins/index.ts
@@ -0,0 +1,12 @@
+export { default as activeLinksPlugin } from './active-links/active-links.js';
+export { default as anchorHeadingsPlugin } from './anchor-headings/anchor-headings.js';
+export { default as beautifyPlugin } from './beautify/beautify.js';
+export { default as copyCodePlugin } from './copy-code/copy-code.js';
+export { default as externalLinksPlugin } from './external-links/external-links.js';
+export { default as highlightCodePlugin } from './highlight-code/highlight-code.js';
+export { default as iconAddonPlugin } from './icon-addon/icon-addon.js';
+export { default as searchPlugin } from './search/search.js';
+export { default as smartQuotesPlugin } from './smart-quotes/smart-quotes.js';
+export { default as smoothLinksPlugin } from './smooth-links/smooth-links.js';
+export { default as tableOfContentsPlugin } from './table-of-contents/table-of-contents.js';
+export { default as tableScrollPlugin } from './table-scroll/table-scroll.js';
